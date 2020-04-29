85f5ba84cc08596a7e2c12ab36fbf5dafddeebd0
Dirk Einecke <dirk.einecke@gmail.com>
2020-04-29 16:16:07 +0200

    Update functions.js

--- robin/js/functions.js
+++ robin/js/functions.js
@@ -1,3 +1,17 @@
-const openMessageModal = function(){
+const openMessageModal = function() {
   $('#messageModal').modal('show');
+};
+
+const openMessageModalQuestion = function(element) {
+  document.querySelector('#messageModal .modal-title').innerHTML = element.dataset.messagetype;
+  document.querySelector('#messageModal .modal-body').innerHTML = element.dataset.messagetext;
+  document.querySelector('#messageModal .action-yes').href = element.dataset.url;
+
+  document.querySelector('#messageModal .action-no').classList.remove('d-none');
+  document.querySelector('#messageModal .action-yes').classList.remove('d-none');
+  document.querySelector('#messageModal .action-close').classList.add('d-none');
+
+  openMessageModal();
+
+  return false;
 };
